3965ee973dfbc0f47636aa75ca77ca07711eb124
fudoge <kchawono@naver.com>
2024-01-09 18:00:56 +0900

    페이지 네비게이션 바 꾸미기, 왼쪽 사이드바의 카테고리 강조 시스템(반쪽짜리)

---
+++ images/script.js
@@ -0,0 +1,12 @@
+// window.onload = function () {
+//     const curr = location.pathname;
+
+//     const target_arr = curr.split('/');
+// }
+
+
+const target_arr = ['PS'];
+
+console.log(target_arr.includes('PS'));
+
+console.log('PS');
